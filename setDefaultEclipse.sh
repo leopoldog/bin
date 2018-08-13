70a738423a08a41122e37dedf35f4f71471b29b4
Leopoldo Ghielmetti <leopoldo.ghielmetti@soprasteria.com>
2018-08-13 13:10:44 +0200

    Use a defined configuration per project.

--- setDefaultEclipse.sh
+++ setDefaultEclipse.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ECLIPSE_DEFAULT="${0/.sh/.csv}"
-
 # check dependencies
 for def in $PROGRAM_NAME crudini
 do
@@ -20,23 +18,24 @@ do
   fi
 done
 
-LIST=$(sed "s/ *#.*//g" ${ECLIPSE_DEFAULT} | cut -d$'\t' -f1 | sort -u)
 
-find . -name "org.eclipse.core.runtime" -type d | grep "/.metadata/.plugins/org.eclipse.core.runtime" \
-| while read location
+find . -name ".default.settings" -type l \
+| while read settings
   do
+    location=$(dirname "$settings")
+    LIST=$(sed "s/ *#.*//g" "$settings" | cut -d$'\t' -f1 | sort -u)
     echo -e "\e[38;5;245mChanging: \e[01;33m$location\e[00m"
 
-    sed "s/ *#.*//g" ${ECLIPSE_DEFAULT} \
+    sed "s/ *#.*//g" "$settings" \
     | grep -v "^$" \
     | sed "s/\\\\/\\\\\\\\/g" \
     | while read file key value
     do
-      $CRUDINI --verbose --set "$location/.settings/$file" "" "$key" "$value" 2>&1 | grep -wv "^unchanged:"
+      $CRUDINI --verbose --set "$location/.metadata/.plugins/org.eclipse.core.runtime/.settings/$file" "" "$key" "$value" 2>&1 | grep -wv "^unchanged:"
     done
 
     for file in $LIST
     do
-      sed -i "s/  *= */=/" "$location/.settings/$file"
+      sed -i "s/  *= */=/" "$location/.metadata/.plugins/org.eclipse.core.runtime/.settings/$file"
     done
   done
